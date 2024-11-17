5d256acb9c338be6db648acdfef961a2b44e7273
sammy-9930 <samanthamenezes99@gmail.com>
2024-11-17 18:39:29 -0500

    adding contains duplicates

--- Arrays_and_hashing/contains_duplicates.py
+++ Arrays_and_hashing/contains_duplicates.py
@@ -1,3 +1,26 @@
+"""
+Brute force approach 
+time complexity: O(n2)
+space complexity: O(1)
+"""
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True 
+        return False 
+
+
+"""
+Hash Map implementation
+time complexity: O(n)
+space complexity: O(n)
+"""
 class Solution(object):
     def containsDuplicate(self, nums):
         """
@@ -12,3 +35,39 @@ class Solution(object):
                 return True 
         return False 
         
+
+"""
+Hash set implementation
+time complexity: O(n)
+space complexity: O(n)
+"""
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
+        for number in nums:
+            if number in seen:
+                return True 
+            seen.add(number)
+        return False
+
+
+"""
+Sorting 
+time complexity: O(nlogn)
+space complexity: O(1) or O(n) depending on sorting algorithm
+"""        
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True 
+        return False 
