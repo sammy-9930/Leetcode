697c095d76f76f97c6b738af4bfbf321af288b0c
sammy-9930 <samanthamenezes99@gmail.com>
2024-11-21 08:20:55 -0500

    adding optimized hashmap implementation

--- Arrays_and_hashing/valid_anagrams.py
+++ Arrays_and_hashing/valid_anagrams.py
@@ -59,3 +59,28 @@ class Solution(object):
             count_s[s[i]] = 1 + count_s.get(s[i], 0)
             count_t[t[i]] = 1 + count_t.get(t[i], 0)
         return count_s == count_t
+
+"""
+Hashtable optimum 
+time complexity: O(n+m) n,m are length of string s and t
+space complexity: O(1) atmost 26 different characters 
+"""
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False 
+        count = [0] * 26 
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1 
+            count[ord(t[i]) - ord('a')] -= 1 
+        for value in count:
+            if value > 0:
+                return False 
+        return True 
+        
+        
