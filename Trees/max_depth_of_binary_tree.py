f22a8dcff7f886f3598654c2da94c818ec9debb7
sammy-9930 <samanthamenezes99@gmail.com>
2024-11-27 21:15:13 -0500

    depth of binary tree

---
+++ Trees/max_depth_of_binary_tree.py
@@ -0,0 +1,26 @@
+"""
+Recursive DFS 
+time complexity: O(n)
+space complexity: O(n)
+"""
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0 
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+"""
+iterative BFS 
+time complexity:
+space complexity: 
+"""
