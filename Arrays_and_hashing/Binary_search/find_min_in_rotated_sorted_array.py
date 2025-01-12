8b4bb7117f5d988b1c012b1d9c7bc9dee1146db0
sammy-9930 <samanthamenezes99@gmail.com>
2025-01-12 14:02:59 -0500

    Find minimum is rotated sorted array

--- Arrays_and_hashing/Binary_search/find_min_in_rotated_sorted_array.py
+++ Arrays_and_hashing/Binary_search/find_min_in_rotated_sorted_array.py
@@ -1,3 +1,4 @@
+"""Iterative approach"""
 def find_min_in_array(s):
     beg, end = 0, len(s) - 1 
     while beg < end:
@@ -10,3 +11,17 @@ def find_min_in_array(s):
 
 nums = [4,5,6,7,0,1,2] 
 print(find_min_in_array(nums))
+
+"""Recursive approach"""
+class Solution:
+    def find(self, beg, end, nums):
+        if beg >= end:
+            return nums[end]
+        mid = (beg + end)//2 
+        if nums[mid] > nums[end]:
+            return self.find(mid+1, end, nums)
+        else:
+            return self.find(beg, mid, nums)
+
+    def findMin(self, nums: List[int]) -> int:
+        return self.find(0, len(nums)-1, nums)
