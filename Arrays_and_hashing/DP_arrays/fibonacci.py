9b54348e0e2f83cde8bc43bbfadce62f74296552
sammy-9930 <samanthamenezes99@gmail.com>
2025-01-21 13:35:05 -0500

    DP Iterative fibonacci

--- Arrays_and_hashing/DP_arrays/fibonacci.py
+++ Arrays_and_hashing/DP_arrays/fibonacci.py
@@ -1,5 +1,5 @@
 """
-Recursion 
+Recursion - top down
 Time complexity: O(2^n)
 Space complexity: O(n)
 """
@@ -36,4 +36,42 @@ class Solution(object):
         values = [-1] * (n+1)
         return self.helper(n, values)
 
+"""
+Iterative approach - bottom up
+Time complexity: O(n)
+Space complexity: O(n) - only array
+"""  
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 1:
+            return n
+        res = [-1] * (n+1)
+        res[0], res[1] = 0, 1 
+        for i in range(2, n+1):
+            res[i] = res[i-1] + res[i-2]
+        return res[n]
+
+"""
+Time complexity : O(n)
+Space complexity : O(1)
+
+"""     
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 1:
+            return n 
+        first_val, sec_val = 0, 1 
+        for i in range(2, n+1):
+            res = first_val + sec_val
+            first_val = sec_val
+            sec_val = res 
+        return res
         
