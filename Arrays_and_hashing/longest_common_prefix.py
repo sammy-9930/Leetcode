6bfb000450cf3b9389ec43757bb49df03e2655e2
sammy-9930 <samanthamenezes99@gmail.com>
2025-01-27 23:31:02 -0500

    Adding time and space complexity

--- Arrays_and_hashing/longest_common_prefix.py
+++ Arrays_and_hashing/longest_common_prefix.py
@@ -1,3 +1,12 @@
+from typing import List
+
+"""
+Time complexity: O(n∗m)
+m - no of strings 
+n - length of shortest word
+Space complexity: O(1)
+"""
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
