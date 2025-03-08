81f3da1cafe80fd625edc59727d4c6857ed9b764
sammy-9930 <samanthamenezes99@gmail.com>
2025-03-08 10:34:45 -0500

    Rotate array - O(n) and O(1) space

--- Arrays_and_hashing/Two_pointers/rotate_arrays.py
+++ Arrays_and_hashing/Two_pointers/rotate_arrays.py
@@ -10,8 +10,42 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        res = [0]*len(nums)
+        res = [-1]*len(nums)
         for i in range(len(nums)):
-            pos = i+k
-            res[pos % len(nums)] = nums[i]
-        nums[:] = res
+            if (i + k) >= len(nums) - 1:
+                pos = (i + k) % len(nums)
+            else:
+                pos = (i + k)
+            res[pos] = nums[i]
+        nums[:] = res 
+
+"""
+Optimized approach
+Time complexity: O(n)
+Space complexity: O(1)
+"""
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return nums
+        # handles case where k > len(nums)
+        k = k % len(nums)
+        def helper(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l = l + 1 
+                r = r - 1 
+            return nums
+        # Step 1: Reverse the entire array
+        helper(0, len(nums)-1)
+        # Step 2: Reverse first k elements
+        helper(0, k - 1)
+        # Step 3: Reverse the rest
+        helper(k, len(nums)-1)
+
+        
+        
+        
