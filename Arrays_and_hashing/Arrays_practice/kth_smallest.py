5fef19fa45ac936239a8cdac8a4ae23157ead208
sammy-9930 <samanthamenezes99@gmail.com>
2025-05-24 18:28:28 -0400

    Kth smallest element in an array

--- Arrays_and_hashing/Arrays_practice/kth_smallest.py
+++ Arrays_and_hashing/Arrays_practice/kth_smallest.py
@@ -8,14 +8,12 @@ print(k_smallest(arr, k))
 
 import heapq
 def k_smallest(arr, k):
-    max_heap = []
-    for number in arr:
-        heapq.heappush(max_heap, -number)
-        if len(max_heap) > k:
-            heapq.heappop(max_heap)
-    return -max_heap[0]
+    heapq.heapify(arr)
+    for _ in range(k-1):
+        heapq.heappop(arr)
+    return heapq.heappop(arr)
     
 
 arr = [7, 10, 4, 3, 20, 15]
-k = 3
+k = 5
 print(k_smallest(arr, k))
